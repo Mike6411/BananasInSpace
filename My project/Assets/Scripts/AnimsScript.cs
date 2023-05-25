962f0985c400477916620b480cc1f31848fce486
Miquel Nadal <miquelnadalmudarra@gmail.com>
2023-05-25 11:13:00 +0200

    Animaciones en un script a parte

---
+++ My project/Assets/Scripts/AnimsScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AnimsScript : MonoBehaviour
+{
+    private Animator anim;
+
+
+    private void Start()
+    {
+        anim = gameObject.GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*Anim Checks*/
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetInteger("AnimationPar", 1);
+        }
+        else
+        {
+            anim.SetInteger("AnimationPar", 0);
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetInteger("AnimationPar", 2);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetInteger("AnimationPar", 3);
+        }
+    }
+}
